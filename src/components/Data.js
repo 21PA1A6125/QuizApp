ec53c67d3feacfbdd2c53e5672ebaed9219271b8
21PA1A6125 <126564390+21PA1A6125@users.noreply.github.com>
2024-09-01 20:28:30 +0530

    Add files via upload

---
+++ src/components/Data.js
@@ -0,0 +1,90 @@
+export const Data=[
+    {
+        q:"Who is the PM of India ?",
+        a:"Narendra Modi",
+        b:"Jawaharlal Nehru",
+        c:"Rahul Gandhi",
+        d:"None",
+        ans:"Narendra Modi"
+    },{
+        q: "Who is the current President of the United States?",
+        a: "Joe Biden",
+        b: "Donald Trump",
+        c: "Barack Obama",
+        d: "George W. Bush",
+        ans: "Joe Biden"
+    },
+    {
+        q: "Which planet is known as the Red Planet?",
+        a: "Venus",
+        b: "Mars",
+        c: "Jupiter",
+        d: "Saturn",
+        ans: "Mars"
+    },
+    {
+        q: "What is the capital city of Japan?",
+        a: "Beijing",
+        b: "Seoul",
+        c: "Tokyo",
+        d: "Bangkok",
+        ans: "Tokyo"
+    },
+    {
+        q: "Who wrote the national anthem of India?",
+        a: "Rabindranath Tagore",
+        b: "Bankim Chandra Chatterjee",
+        c: "Subhash Chandra Bose",
+        d: "Sarojini Naidu",
+        ans: "Rabindranath Tagore"
+    },
+    {
+        q: "Which is the largest continent in the world?",
+        a: "Africa",
+        b: "Europe",
+        c: "Asia",
+        d: "Australia",
+        ans: "Asia"
+    },
+    {
+        q: "Which is the longest river in the world?",
+        a: "Amazon",
+        b: "Nile",
+        c: "Yangtze",
+        d: "Mississippi",
+        ans: "Nile"
+    },
+    {
+        q: "What is the chemical symbol for water?",
+        a: "O2",
+        b: "H2O",
+        c: "CO2",
+        d: "NaCl",
+        ans: "H2O"
+    },
+    {
+        q: "Who discovered penicillin?",
+        a: "Louis Pasteur",
+        b: "Marie Curie",
+        c: "Alexander Fleming",
+        d: "Isaac Newton",
+        ans: "Alexander Fleming"
+    },
+    {
+        q: "What is the largest mammal in the world?",
+        a: "Elephant",
+        b: "Blue Whale",
+        c: "Great White Shark",
+        d: "Giraffe",
+        ans: "Blue Whale"
+    },
+    {
+        q: "Which Indian city is known as the 'Pink City'?",
+        a: "Udaipur",
+        b: "Jodhpur",
+        c: "Jaipur",
+        d: "Bikaner",
+        ans: "Jaipur"
+    }    
+    
+]
